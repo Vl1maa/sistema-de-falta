d9216ddf518a5d1e11f5cec7d814bc9d4a4de730
Vinicius Lima <vini.spec38@gmail.com>
2023-03-27 19:57:50 -0300

    Corrgindo erro

--- src/back/sistema-falta/src/main/java/com/api/sistemafalta/AutenticacaoController.java
+++ src/back/sistema-falta/src/main/java/com/api/sistemafalta/AutenticacaoController.java
@@ -18,24 +18,28 @@ public class AutenticacaoController {
     NamedParameterJdbcTemplate jdbcTemplate;
 
     @PostMapping(value = "/login")
-    public ResponseEntity<?> novo(@RequestBody LoginDTO login) {
-        try {
-            String query = "SELECT * FROM usuario WHERE usuario = :usuario LIMIT 1;";
-            MapSqlParameterSource params = new MapSqlParameterSource();
-            params.addValue("usuario", login.getUsuario());
-            Response status = jdbcTemplate.query(query, params, rs -> {
-                if (rs.next()) {
-                    if (rs.getString("senha").equals(login.getSenha())) {
-                        return new Response(rs.getString("usuario"), 200);
-                    } else {
-                        return new Response("Dados incorretos de login", 401);
-                    }
+public ResponseEntity<?> novo(@RequestBody LoginDTO login) {
+    try {
+        String query = "SELECT * FROM usuario WHERE usuario = :usuario LIMIT 1;";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("usuario", login.getUsuario());
+        Response status = jdbcTemplate.query(query, params, rs -> {
+            if (rs.next()) {
+                if (rs.getString("senha").equals(login.getSenha())) {
+                    return new Response(rs.getString("usuario"), 200);
+                } else {
+                    return new Response("Dados incorretos de login", 401);
                 }
-                return null;
-            });
+            }
+            return null;
+        });
+        if (status != null) {
             return ResponseEntity.status(status.getStatusCode()).body(status);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        } else {
+            return ResponseEntity.badRequest().body("Nenhum usuário encontrado");
         }
+    } catch (Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
+}
 }
